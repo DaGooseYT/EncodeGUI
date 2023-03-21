855eb7a8b6c7a00896275af373c17d94910fd7ad
Goose <72713589+DaGooseYT@users.noreply.github.com>
2023-03-21 06:15:16 -0400

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "UI\EncodeGUI.h"
-#include <QtWidgets/QApplication>
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    EncodeGUI w;
-    w.show();
-    return a.exec();
-}
