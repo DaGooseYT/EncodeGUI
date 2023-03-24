4fb5612e936a542360b0d7a2aaeba9088e4534b7
Goose <72713589+DaGooseYT@users.noreply.github.com>
2023-03-24 10:13:01 -0400

    Update ui_updater.h

--- x64/Release/uic/ui_updater.h
+++ x64/Release/uic/ui_updater.h
@@ -1,11 +1,3 @@
-/********************************************************************************
-** Form generated from reading UI file 'updater.ui'
-**
-** Created by: Qt User Interface Compiler version 6.2.4
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
-********************************************************************************/
-
 #ifndef UI_UPDATER_H
 #define UI_UPDATER_H
 
