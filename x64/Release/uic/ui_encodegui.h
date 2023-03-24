354765e0143b711f19bbaee87b5f8968e7873800
Goose <72713589+DaGooseYT@users.noreply.github.com>
2023-03-24 10:12:22 -0400

    Update ui_encodegui.h

--- x64/Release/uic/ui_encodegui.h
+++ x64/Release/uic/ui_encodegui.h
@@ -1,11 +1,3 @@
-/********************************************************************************
-** Form generated from reading UI file 'encodegui.ui'
-**
-** Created by: Qt User Interface Compiler version 6.2.4
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
-********************************************************************************/
-
 #ifndef UI_ENCODEGUI_H
 #define UI_ENCODEGUI_H
 
